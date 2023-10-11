5d5dd058554196d15881933e6a9eaa2120248f31
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-10-11 12:17:37 +0200

    Aggiungere i file di progetto.

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Category() { }
+
+
+        public List<Photo>? Photos { get; set; }
+
+    }
+}
